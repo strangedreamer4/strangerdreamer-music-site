0acc24ca7155c0331065dc23ae2091f3bc97ea85
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-09-09 22:31:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './public/index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyberpunkPink: '#ff007f',
+        cyberpunkBlue: '#00bfff',
+        cyberpunkPurple: '#800080',
+        cyberpunkGreen: '#00ff00',
+      },
+      fontFamily: {
+        cyberpunk: ['Orbitron', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-};
+}

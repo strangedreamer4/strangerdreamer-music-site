3eb7ec280611880065ba65a52d4d0d6fa412d30b
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-09-09 22:48:46 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar bg-gray-800 p-4 text-white">
-      <div className="container mx-auto">
-        <Link to="/" className="mr-4 hover:text-cyberpunkPink">Home</Link>
-        <Link to="/about" className="mr-4 hover:text-cyberpunkPink">About</Link>
-        <Link to="/contact" className="hover:text-cyberpunkPink">Contact</Link>
+    <nav className="navbar bg-gray-900 p-4 text-white shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-cyberpunkPink text-2xl hover:text-cyberpunkBlue">StRaNgErDrEaMeR</Link>
+        <div>
+          <Link to="/" className="mr-4 hover:text-cyberpunkPink">Home</Link>
+          <Link to="/about" className="mr-4 hover:text-cyberpunkPink">About</Link>
+          <Link to="/contact" className="hover:text-cyberpunkPink">Contact</Link>
+        </div>
       </div>
     </nav>
   );

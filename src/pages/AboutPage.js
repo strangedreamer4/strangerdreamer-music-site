680697a0d3eb08ca7951312d141d3e31a6800672
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-09-09 22:34:40 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div className="about-page container mx-auto mt-10">
-      <h1 className="text-4xl font-bold text-center mb-8">About StRaNgErDrEaMeR</h1>
-      <p className="text-lg text-center">
+    <div className="about-page container mx-auto mt-10 text-center">
+      <h1 className="text-4xl font-bold mb-8 text-cyberpunkPink">About StRaNgErDrEaMeR</h1>
+      <p className="text-lg">
         StRaNgErDrEaMeR is a visionary artist blending dreamy melodies with soulful beats.
         Welcome to the world of his music.
       </p>
